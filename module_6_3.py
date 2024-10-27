0efb9119033a8346bc86592aa89ea10b28d2e831
OlgaAL525 <mouse5525@gmail.com>
2024-10-28 01:39:26 +0300

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,40 @@
+class Horse:
+    x_distanse = 0
+    sound = 'Frrr'
+
+    def run (self, dx):
+        self.x_distanse += dx
+
+
+class Eagle:
+    y_distanse = 0
+    sound = 'I train, eat, sleep and repeat'
+
+    def fly(self, dy):
+        self.y_distanse += dy
+
+
+class Pegasus(Horse, Eagle):
+
+    def move(self, dx, dy):
+        super().run(dx)
+        super().fly(dy)
+
+    def get_pos(self):
+        return (self.x_distanse, self.y_distanse)
+
+    def voice(self):
+        print(Eagle.sound)
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+
+p1.move(10,15)
+print(p1.get_pos())
+
+p1.move(-5,20)
+print(p1.get_pos())
+
+p1.voice()
